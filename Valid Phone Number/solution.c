d972a8907fd7bdb4c68a48cba8dcc87c1aab613d
Angel de la Torre <angel.dlt.1996@gmail.com>
2019-12-18 15:31:54 -0800

    added best solutions to problem

--- Valid Phone Number/solution.c
+++ Valid Phone Number/solution.c
@@ -27,3 +27,41 @@ bool valid_phone_number(const char* number) {
     }
     return true;
 }
+
+//Best Solutions
+
+// sscanf solution
+/* 
+#include <stdbool.h>
+
+bool valid_phone_number(const char* number)
+{
+   int frmt_len = strlen("(123) 456-7890");
+   if (strlen(number) == frmt_len)
+   {
+      char b[frmt_len];
+      if (sscanf(number, "%1[(]%3[0-9]%1[)]%1[ ]%3[0-9]%1[-]%4[0-9]%1s", b, b, b, b, b, b, b, b) == 7) return true;
+   }
+   return false;
+}
+*/
+
+// Macro solution
+/* 
+#include <stdbool.h>
+#include <ctype.h>
+
+bool valid_phone_number(const char* number) {
+#define SCAN(c)   if (*number++ != c) { return false; }
+#define SCAN_D(n) for (int i = 0; i < n; i += 1) { if (!isdigit(*number++)) { return false; } }
+    SCAN('(');
+    SCAN_D(3);
+    SCAN(')');
+    SCAN(' ');
+    SCAN_D(3);
+    SCAN('-');
+    SCAN_D(4);
+    SCAN('\0');
+    return true;
+} 
+*/
